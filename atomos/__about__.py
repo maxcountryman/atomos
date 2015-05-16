9e84647bfad870ff708e7a865935882353f41628
Max Countryman <maxc@me.com>
2015-05-16 11:20:18 -0700

    version 0.3.0

--- atomos/__about__.py
+++ atomos/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __title__ = 'atomos'
-__version_info__ = ('0', '2', '0')
+__version_info__ = ('0', '3', '0')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Max Countryman'
 __license__ = 'BSD'
